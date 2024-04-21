c5db2bec7235d6230d82366c82d69c13777bb79a
Paul Williams <paul58914080@gmail.com>
2024-04-21 22:16:06 +0530

    build: integrate jest as test runner

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ff4j-web');
+    expect(compiled.querySelector('span')?.textContent).toContain('Hello, ff4j-web');
   });
 });
